09d7cd07710da423e7eeeb8a28d8e88b799e2b2b
github username <github email address>
2016-07-12 14:39:16 -0400

    lab completed

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -17,6 +17,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      write your function here
      
      */
-
+    
+    func solveTrivia(statesAndCapitals: [String:String]) -> String {
+        for (state, capital) in statesAndCapitals {
+            var characterCounter = 0
+            for stateCharacter in state.lowercaseString.characters {
+                let str = String(stateCharacter)
+                if capital.containsString(str) {
+                    break;
+                }
+                characterCounter += 1
+            }
+            if characterCounter == state.characters.count {
+                return state
+            }
+        }
+        return "state not found"
+    }
 }
 
